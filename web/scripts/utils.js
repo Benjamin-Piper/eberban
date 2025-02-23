dce97ef1b7177f8efb456eaca8b629c69a073b8a
Benjamin Piper <68324342+Benjamin-Piper@users.noreply.github.com>
2025-02-23 20:27:24 +1100

    🚚 Reintroduce eberban root generator (4/

---
+++ web/scripts/utils.js
@@ -0,0 +1,3 @@
+export function get_random_item(list) {
+    return list[Math.floor(Math.random() * list.length)];
+}
--- web/vite.config.js
+++ web/vite.config.js
@@ -1,3 +1,6 @@
+import ViteYaml from "@modyfi/vite-plugin-yaml";
+
 export default {
+    plugins: [ViteYaml()],
     publicDir: "images",
-}
+};
