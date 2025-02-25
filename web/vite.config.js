7238e7c59f26869baa37704cf6d841040cabfb07
Benjamin Piper <68324342+Benjamin-Piper@users.noreply.github.com>
2025-02-25 20:40:56 +1100

    vite dev will intentionally be 404 for refgram

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,8 +1,7 @@
 import { dirname, resolve } from "node:path"
 import { fileURLToPath } from "node:url"
+import { defineConfig } from "vite"
 import ViteYaml from "@modyfi/vite-plugin-yaml";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // Adapted from https://github.com/vitejs/vite/issues/6596#issuecomment-1651355986
 // Note: this only changes the request, it does NOT change the url on the browser.
@@ -39,17 +38,19 @@ const AppendTrailingUrlSlash = () => {
     }
 }
 
-export default {
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
     appType: "mpa", // multi-page application
     build: {
         rollupOptions: {
-          input: {
-            "main": resolve(__dirname, "index.html"),
-            "root-generator": resolve(__dirname, "root-generator/index.html"),
+            input: {
+                "main": resolve(__dirname, "index.html"),
+                "root-generator": resolve(__dirname, "root-generator/index.html"),
             },
         },
     },
     plugins: [AppendTrailingUrlSlash(), ViteYaml()],
-    publicDir: "../images", // need to go one level up because our root is at src.
     root: "src",
-};
+    publicDir: "../images",
+});
