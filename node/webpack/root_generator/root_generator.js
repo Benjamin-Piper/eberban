defa5bb30b4887bc661c00b5a5471bec2cc09629
Benjamin Piper <68324342+Benjamin-Piper@users.noreply.github.com>
2025-03-03 11:08:45 +1100

    Squashed commit of the following:
    commit fe5fb7b7eedfcabe5c7f212083b71238420db4c9
    Author: Benjamin Piper <68324342+Benjamin-Piper@users.noreply.github.com>
    Date:   Mon Mar 3 11:07:32 2025 +1100
    
        more wip
    
    commit 149117256dc607a37a6cc8dc5f8e78a9ad9a6ad1
    Author: Benjamin Piper <68324342+Benjamin-Piper@users.noreply.github.com>
    Date:   Sat Feb 22 20:56:17 2025 +1100
    
        more wip
    
    commit bb3cbaa98f96e73100d2a547b821af45e08efa2a
    Author: Benjamin Piper <68324342+Benjamin-Piper@users.noreply.github.com>
    Date:   Wed Feb 19 21:10:51 2025 +1100
    
        wip. getting there
    
    commit d9e412501ac8d65fa5d25c5773f48dbde524fd56
    Author: Benjamin Piper <68324342+Benjamin-Piper@users.noreply.github.com>
    Date:   Wed Feb 19 21:09:42 2025 +1100
    
        more common.css stuff
    
    commit ee516bc5607fd40cc5150b41ba49ee3cae9b6082
    Author: Benjamin Piper <68324342+Benjamin-Piper@users.noreply.github.com>
    Date:   Mon Feb 17 21:25:29 2025 +1100
    
        wip
    
    commit 0688f84422d3129b9500333d2d8ad0fd826cf434
    Author: Benjamin Piper <68324342+Benjamin-Piper@users.noreply.github.com>
    Date:   Mon Feb 17 21:24:59 2025 +1100
    
        reset css stuff
    
    commit 6d24de6a2daf63c9f243db19e6235265bb1ef138
    Author: Benjamin Piper <68324342+Benjamin-Piper@users.noreply.github.com>
    Date:   Mon Feb 17 21:24:41 2025 +1100
    
        common css stuff
    
    commit 31fd7056a0de289e4e043bd4794afa5f41cd44e4
    Author: Benjamin Piper <68324342+Benjamin-Piper@users.noreply.github.com>
    Date:   Mon Feb 17 12:15:24 2025 +1100
    
        WIP
    
    commit d088e774c7fb508695f7ddc40561e0c816364500
    Author: Benjamin Piper <68324342+Benjamin-Piper@users.noreply.github.com>
    Date:   Mon Feb 17 12:15:14 2025 +1100
    
        🚚💄 Move common styles to common.css
    
    commit 7a721aa6482d8462ea9bd5808c54c48ecc428667
    Author: Benjamin Piper <68324342+Benjamin-Piper@users.noreply.github.com>
    Date:   Mon Feb 17 12:13:43 2025 +1100
    
        🚚💄 Move reset styles to reset.css

---
+++ node/webpack/root_generator/root_generator.js
@@ -0,0 +1,98 @@
+const { dictionary_en: dictionary } = require('../../src/dictionary');
+const { 
+    get_random_three_letter_intransitive_root,
+    get_random_four_letter_intransitive_root,
+    get_random_five_letter_intransitive_root,
+    get_random_six_letter_intransitive_root,
+    get_random_three_letter_transitive_root,
+    get_random_four_letter_transitive_root,
+    get_random_five_letter_transitive_root,
+    get_random_six_letter_transitive_root,
+} = require('../../src/root_generation/index');
+// export default dictionary;
+
+
+/* Transitivity */
+
+
+const is_intransitive = (s) => s === "intransitive";
+
+const is_transitive = (s) => s === "transitive";
+
+const is_valid = (s) => is_intransitive(s) || is_transitive(s);
+
+function get_transitivity(transitive_string) {
+    // default to intransitive 
+    for (const transitivity of [transitive_string, localStorage.getItem("transitivity"), "intransitive"]) {
+        if (is_valid(transitivity)) {
+            return transitivity;
+        }
+    }
+}
+
+
+/* Buttons */
+
+
+export function update_buttons(transitivity) {
+    const intransitiveButton = document.getElementById("intransitive");
+    const transitiveButton = document.getElementById("transitive");
+    if (is_transitive(transitivity)) {
+        transitiveButton.disabled = true;
+        intransitiveButton.disabled = false;
+    } else {
+        transitiveButton.disabled = false;
+        intransitiveButton.disabled = true;
+    }
+}
+
+
+/* Root generation */
+
+
+function* generate_roots(root_fn) {
+    while (true) {
+        const word = root_fn();
+        if (!dictionary[word]) {
+            yield word;
+        }
+    }
+}
+
+function formatRoot(root) {
+    const element = document.createElement("LI");
+    element.innerText = root;
+    return element;
+}
+
+function update_root_list(root_list_id, root_fn) {
+    const root_list = document.getElementById(root_list_id);
+    root_list.innerHTML = null;
+    const generator = generate_roots(root_fn);
+    for (let i = 0; i <= 10; i++) {
+        const root = generator.next().value;
+        root_list.appendChild(formatRoot(root));
+    }
+}
+
+
+/* ENTRY */
+
+
+export function refreshy(transitive_string) {
+    const transitivity = get_transitivity(transitive_string);
+    update_buttons(transitivity);
+    if (is_transitive(transitivity)) {
+        update_root_list("short-roots", get_random_three_letter_transitive_root);
+        update_root_list("medium-roots", get_random_four_letter_transitive_root);
+        update_root_list("long-roots", get_random_five_letter_transitive_root);
+        update_root_list("longer-roots", get_random_six_letter_transitive_root);
+    } else {
+        update_root_list("short-roots", get_random_three_letter_intransitive_root);
+        update_root_list("medium-roots", get_random_four_letter_intransitive_root);
+        update_root_list("long-roots", get_random_five_letter_intransitive_root);
+        update_root_list("longer-roots", get_random_six_letter_intransitive_root);
+    }
+    localStorage.setItem("transitivity", transitivity);
+    
+}
