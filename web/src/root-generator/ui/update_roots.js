30f3edfe8a92382b72c240daa48ca027bc8d3254
Benjamin Piper <68324342+Benjamin-Piper@users.noreply.github.com>
2025-03-10 09:22:30 +1100

    WIP on roots caching (need a way to switch between easily without updating roots)

--- web/src/root-generator/ui/update_roots.js
+++ web/src/root-generator/ui/update_roots.js
@@ -11,75 +11,136 @@ import {
 } from "../logic/random_root";
 
 
-/* Transitivity */
+/* Helpers */
 
 
 const is_intransitive = (s) => s === "intransitive";
 const is_transitive = (s) => s === "transitive";
 const is_valid = (s) => is_intransitive(s) || is_transitive(s);
 
-function set_transitivity(transitive_string) {
-    const transitivityList = [
-        transitive_string,
-        localStorage.getItem("transitivity"),
-        "intransitive", // Default to intransitive.
-    ];
-    for (const transitivity of transitivityList) {
-        if (is_valid(transitivity)) {
-            localStorage.setItem("transitivity", transitivity);
-            return transitivity;
+
+/* Root Lists */
+
+
+class Root_Lists {
+    #root_lists;
+    #transitivity;
+
+    constructor() {
+        this.#initialise_transitivity();
+        this.#render_transitivity();
+        this.#initialise_root_lists();
+        this.#render_roots();
+    }
+
+
+    /* Roots */
+
+
+    #initialise_root_lists() {
+        const cached_root_lists = JSON.parse(localStorage.getItem("root-lists"));
+        if (cached_root_lists) {
+            this.#root_lists = cached_root_lists;
+            return;
+        }
+        this.#root_lists = [
+            {
+                id: "short-roots",
+                roots: [],
+                transitive_fn: get_random_three_letter_transitive_root,
+                intransitive_fn: get_random_three_letter_intransitive_root,
+            },
+            {
+                id: "medium-roots",
+                roots: [],
+                transitive_fn: get_random_four_letter_transitive_root,
+                intransitive_fn: get_random_four_letter_intransitive_root,
+            },
+            {
+                id: "long-roots",
+                roots: [],
+                transitive_fn: get_random_five_letter_transitive_root,
+                intransitive_fn: get_random_five_letter_intransitive_root,
+            },
+            {
+                id: "longer-roots",
+                roots: [],
+                transitive_fn: get_random_six_letter_transitive_root,
+                intransitive_fn: get_random_six_letter_intransitive_root,
+            },
+        ];
+        this.#update_roots();
+    }
+
+    refresh() {
+        this.#update_roots();
+        this.#render_roots();
+    }
+
+    #render_roots() {
+        const format_root = (root) => {
+            const element = document.createElement("LI");
+            element.innerText = root;
+            return element;
+        }
+        const render_column_element = (root_list_id, roots) => {
+            const column_element = document.getElementById(root_list_id);
+            column_element.innerHTML = null;
+            column_element.append(...roots.map(format_root));
+        }
+        for (const root_list of this.#root_lists) {
+            render_column_element(root_list.id, root_list.roots);
         }
     }
-}
-
-
-/* Buttons */
-
-
-function update_buttons(transitivity) {
-    const intransitive_button = document.getElementById("intransitive");
-    const transitive_button = document.getElementById("transitive");
-    if (is_transitive(transitivity)) {
-        transitive_button.disabled = true;
-        intransitive_button.disabled = false;
-    } else {
-        transitive_button.disabled = false;
-        intransitive_button.disabled = true;
+    
+    #update_roots() {
+        for (const root_list of this.#root_lists) {
+            if (is_transitive(this.#transitivity)) {
+                root_list.roots = get_random_roots(root_list.transitive_fn);
+            } else {
+                root_list.roots = get_random_roots(root_list.intransitive_fn);
+            }
+        }
     }
-}
 
 
-/* Raots */
+    /* Transitivity */
 
 
-function format_root(root) {
-    const element = document.createElement("LI");
-    element.innerText = root;
-    return element;
-}
+    #initialise_transitivity() {
+        const transitivity_list = [
+            localStorage.getItem("transitivity"),
+            "intransitive", // Default to intransitive.
+        ];
+        for (const transitivity of transitivity_list) {
+            if (is_valid(transitivity)) {
+                this.#transitivity = transitivity;
+                return;
+            }
+        } 
+    }
 
-function update_root_list(root_list_id, get_random_root_fn) {
-    const root_list = document.getElementById(root_list_id);
-    root_list.innerHTML = null;
-    root_list.append(...get_random_roots(get_random_root_fn).map(format_root))
+    #set_transitivity(new_transitivity) {
+        this.#transitivity = new_transitivity;
+        localStorage.setItem("transitivity", new_transitivity);
+    }
+
+    // TODO perhaps rename this??
+    switch_transitivity_to(new_transitivity) {
+        this.#set_transitivity(new_transitivity);
+        this.#render_transitivity();
+        this.#render_roots();
+    }
+
+    #render_transitivity() {
+        const intransitive_button = document.getElementById("intransitive");
+        const transitive_button = document.getElementById("transitive");
+        intransitive_button.disabled = is_intransitive(this.#transitivity);
+        transitive_button.disabled = is_transitive(this.#transitivity);
+    }
 }
 
 
 /* ENTRY */
-
-
-export default function update_roots(transitive_string) {
-    const transitivity = set_transitivity(transitive_string);
-    update_buttons(transitivity);
-    if (is_transitive(transitivity)) {
-        update_root_list("short-roots", get_random_three_letter_transitive_root);
-        update_root_list("medium-roots", get_random_four_letter_transitive_root);
-        update_root_list("long-roots", get_random_five_letter_transitive_root);
-        update_root_list("longer-roots", get_random_six_letter_transitive_root);
-    } else {
-        update_root_list("short-roots", get_random_three_letter_intransitive_root);
-        update_root_list("medium-roots", get_random_four_letter_intransitive_root);
-        update_root_list("long-roots", get_random_five_letter_intransitive_root);
-        update_root_list("longer-roots", get_random_six_letter_intransitive_root);
-    }   
-}
+const root_lists = new Root_Lists();
+export default root_lists;
